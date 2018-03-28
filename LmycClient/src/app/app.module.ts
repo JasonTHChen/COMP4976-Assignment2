bcaf5a65443fac7165ea27f378cb5a30cc3cd2c4
Jason <jasonccmfry@gmail.com>
2018-03-27 23:07:48 -0700

    setup basic navbar

--- LmycClient/src/app/app.module.ts
+++ LmycClient/src/app/app.module.ts
@@ -4,17 +4,19 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-
+import { BoatsComponent } from './boats/boats.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BoatsComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      { path: '**', component: HomeComponent }
+      { path: '', component: HomeComponent },
+      { path: 'boats', component: BoatsComponent }
     ])
   ],
   providers: [],
